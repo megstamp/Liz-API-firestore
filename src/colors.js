2175a9ab1daf30c58f62c06c9328b9488de25a6d
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-07 17:37:52 -0400

    added firebase

--- src/colors.js
+++
@@ -1,9 +0,0 @@
-import {db} from "./dbconnect.js"
-
-// const coll = db.collection("colors");
-
-export async function addNewColor(req, res) {
-    const newColor = req.body
-    await db.collection("colors")
-    res.status(201).send({message:"it worked!"})
-}
--- src/dbconnect.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp, cert } from "firebase-admin/app"
-
-import {getFirestore} from "firebase-admin/firestore"
-
-import {secrets} from "../secrets.js"
-
-initializeApp ({ credential: cert(secrets)})
-
-export const db = getFirestore ()
